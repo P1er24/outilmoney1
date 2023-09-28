a4039f3c3deefb93833fa94afbbda1b582dd9040
P1er24 <maplec1s24@gmail.com>
2023-09-28 10:56:31 -0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,23 @@
 ﻿// Emmanuel
-Console.WriteLine("Hello, World!");
+
+
+double cadAmount, usdamount, euramount;
+
+Console.WriteLine("hello, customer" + "Please enter you're money value (CAD):");
+if (double.TryParse(Console.ReadLine(), out cadAmount))
+{
+    usdamount = cadAmount * 0.79; //tAUX DE change CAD vers USD
+    euramount = cadAmount * 0.68; //taux de change CAD vers EUR
+
+    object usdAmount = null;
+    Console.WriteLine($"Montant en dollars américains (USD) : {usdAmount:C}");
+    object eurAmount = null;
+    Console.WriteLine($"Montant en euros (EUR) : {eurAmount:C}");
+}
+else
+{
+    Console.WriteLine("Montant invalide. Assurez-vous d'entrer un nombre valide.");
+}
+
+Console.ReadKey();
+//declare before closing
